4496ab0893ef4f2c56d8b246d6e122355307548a
sff1019 <starfield1019@gmail.com>
2020-10-22 23:08:00 +0900

    rename sac example and add wandb

--- examples/run_sac.py
+++ examples/run_sac.py
@@ -9,8 +9,6 @@ if __name__ == '__main__':
     parser = Trainer.get_argument()
     parser = SAC.get_argument(parser)
     parser = WandB.get_argument(parser)
-    parser.add_argument('--alpha', type=float, default=0.2)
-    parser.add_argument('--auto-alpha', action="store_true")
     parser.add_argument('--env-name', type=str, default="Pendulum-v0")
     parser.add_argument('--no_cuda', action='store_true')
     parser.set_defaults(batch_size=256)
@@ -23,14 +21,20 @@ if __name__ == '__main__':
 
     env = gym.make(args.env_name)
     test_env = gym.make(args.env_name)
-    policy = SAC(
-        state_shape=env.observation_space.shape,
-        action_dim=env.action_space.high.size,
-        device=device,
-        max_action=env.action_space.high[0],
-        batch_size=args.batch_size,
-        n_warmup=args.n_warmup,
-        alpha=args.alpha,
-    )
-    trainer = Trainer(policy, env, device, args, test_env=test_env)
+    policy = SAC(state_shape=env.observation_space.shape,
+                 action_dim=env.action_space.high.size,
+                 device=device,
+                 memory_capacity=args.memory_capacity,
+                 max_action=env.action_space.high[0],
+                 batch_size=args.batch_size,
+                 n_warmup=args.n_warmup,
+                 alpha=args.alpha,
+                 auto_alpha=args.auto_alpha)
+    trainer = Trainer(policy,
+                      env,
+                      device,
+                      args,
+                      test_env=test_env,
+                      wandb_turn_on=args.wandb_turn_on,
+                      wandb_configs=wandb_configs)
     trainer()
