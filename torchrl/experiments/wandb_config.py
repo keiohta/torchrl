a0c355749be3f215c1d970ba80cbea9d68a33ad7
Kei Ota <dev.ohtakei@gmail.com>
2021-01-28 08:34:48 +0900

    style: change wandb setting

--- torchrl/experiments/wandb_config.py
+++ torchrl/experiments/wandb_config.py
@@ -7,12 +7,11 @@ class WandB:
         if parser is None:
             parser = argparse.ArgumentParser(conflict_handler='resolve')
         # yapf: disable
-        parser.add_argument('--env-name', type=str, default="CartPole-v0")
+        parser.add_argument('--env-name', type=str, default="Pendulum-v0")
         parser.add_argument('--wandb_turn_on', action='store_true')
-        parser.add_argument('--wandb_entity', type=str, default='sff1019')
-        parser.add_argument('--wandb_project', type=str,
-                            default='reinforcement_learning_algorithms')
-        parser.add_argument('--wandb_run_name', type=str, default='torchrl_dqn_cartpole')
+        parser.add_argument('--wandb_entity', type=str, default='ohtake_i')
+        parser.add_argument('--wandb_project', type=str, default='torchrl')
+        parser.add_argument('--wandb_run_name', type=str, default='torchrl_sac_pendulum')
         parser.add_argument('--wandb_monitor_gym', action='store_true')
         parser.add_argument('--wandb_gif_dir', type=str, default='logs/dqn_cartpole')
         parser.add_argument('--wandb_gif_header', type=str, default='')
